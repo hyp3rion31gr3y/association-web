fb5653c70dded2e975c91791f762bc35c260898b
hyp3rion31gr3y <44115784+hyp3rion31gr3y@users.noreply.github.com>
2020-09-30 17:52:08 +0530

    Update test_version.py

--- src/test_version.py
+++ src/test_version.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pytest
-import django
+import pytest #this imports pytest package
+import django # this imports django package
 
-def test_current_django_version():
+def test_current_django_version(): #def is used to create a function
     assert django.VERSION >= (1, 6, 1)
 
 
